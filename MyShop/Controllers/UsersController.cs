2e7b1431a6e928566e7534f2594f00bf8a4ce271
Ghoust48 <slepuhin.ghoust48@gmail.com>
2020-11-11 20:01:05 +0300

    Added users controller and create, edit pages

---
+++ MyShop/Controllers/UsersController.cs
@@ -0,0 +1,111 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyShop.Models;
+using MyShop.ViewModels;
+
+namespace MyShop.Controllers
+{
+    public class UsersController : Controller
+    {
+        private readonly UserManager<User> _userManager;
+
+        public UsersController(UserManager<User> userManager)
+        {
+            _userManager = userManager;
+        }
+        
+        public IActionResult Index() => View(_userManager.Users.ToList());
+ 
+        public IActionResult Create() => View();
+ 
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User
+                {
+                    Email = model.Email, 
+                    UserName = model.Email,
+                    FirstName = model.FirstName,
+                    LastName = model.LastName
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+            return View(model);
+        }
+ 
+        public async Task<IActionResult> Edit(string id)
+        {
+            User user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            EditUserViewModel model = new EditUserViewModel
+            {
+                Id = user.Id, 
+                Email = user.Email, 
+                FirstName = user.FirstName,
+                LastName = user.LastName
+            };
+            return View(model);
+        }
+ 
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.FindByIdAsync(model.Id);
+                
+                if(user!=null)
+                {
+                    user.Email = model.Email;
+                    user.UserName = model.Email;
+                    user.FirstName = model.FirstName;
+                    user.LastName = model.LastName;
+                     
+                    var result = await _userManager.UpdateAsync(user);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("Index");
+                    }
+                    else
+                    {
+                        foreach (var error in result.Errors)
+                        {
+                            ModelState.AddModelError(string.Empty, error.Description);
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+ 
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            
+            if (user != null)
+            {
+                var result = await _userManager.DeleteAsync(user);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ MyShop/ViewModels/EditUserViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MyShop.ViewModels
+{
+    public class EditUserViewModel
+    {
+        public string Id { get; set; }
+        
+        public string FirstName { get; set; }
+        
+        public string LastName { get; set; }
+        
+        public string Email { get; set; }
+    }
+}
