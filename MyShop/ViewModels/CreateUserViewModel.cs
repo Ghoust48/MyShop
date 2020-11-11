d23d92d39339d96b2890e4d712b8ef979d10f3fd
Ghoust48 <slepuhin.ghoust48@gmail.com>
2020-11-11 20:12:06 +0300

    Updated view models

--- MyShop/ViewModels/CreateUserViewModel.cs
+++ MyShop/ViewModels/CreateUserViewModel.cs
@@ -1,13 +1,25 @@
-﻿namespace MyShop.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.ViewModels
 {
     public class CreateUserViewModel
     {
+        [Required] 
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
-        
+
+        [Required] 
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
         
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public string Email { get; set; }
         
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Пароль")]
         public string Password { get; set; }
     }
 }
--- MyShop/ViewModels/EditUserViewModel.cs
+++ MyShop/ViewModels/EditUserViewModel.cs
@@ -1,13 +1,22 @@
-﻿namespace MyShop.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyShop.ViewModels
 {
     public class EditUserViewModel
     {
         public string Id { get; set; }
         
+        [Required] 
+        [Display(Name = "Имя")]
         public string FirstName { get; set; }
-        
+
+        [Required] 
+        [Display(Name = "Фамилия")]
         public string LastName { get; set; }
         
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
         public string Email { get; set; }
     }
 }
