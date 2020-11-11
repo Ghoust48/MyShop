6dcc027a861414cd10411485d3fc9353dccb8077
Ghoust48 <slepuhin.ghoust48@gmail.com>
2020-11-11 20:36:51 +0300

    Added roles controller and views

---
+++ MyShop/ViewModels/ChangeRoleViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Identity;
+
+namespace MyShop.ViewModels
+{
+    public class ChangeRoleViewModel
+    {
+        public string UserId { get; set; }
+        
+        public string UserEmail { get; set; }
+        
+        public List<IdentityRole> AllRoles { get; set; } = new List<IdentityRole>();
+        
+        public IList<string> UserRoles { get; set; } = new List<string>();
+        
+    }
+}
